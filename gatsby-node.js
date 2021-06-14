46c33516a6268aec5158b3cdd6c221ba12aaa922
Harrison Cramer <harrisoncramer@gmail.com>
2021-06-14 17:01:33 -0400

    Moved from CSS modules to styled components.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,30 +4,30 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-const path = require("path")
+// const path = require("path")
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.tsx`)
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve(`src/templates/blog-post.tsx`)
 
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `)
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-}
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
+// }
