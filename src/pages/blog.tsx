e5c3a5a99db19d48c3c5b9b1d416aef0cde78d1f
Harrison Cramer <harrisoncramer@gmail.com>
2021-06-16 11:57:55 -0400

    Functioning page that displays all blog posts. Added typing for post in separate /types folder.

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,19 +1,59 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import Seo from "../components/seo/seo"
 import { StyledP } from "../components/styled-components/text"
+import { Post } from "../types/markdown"
 
-const BlogPage = (): JSX.Element => (
-  <Layout>
-    <Seo
-      title="harrison.me"
-      lang="en-us"
-      description="This blog contains posts about what I'm learning as a software engineer. Topics include Javascript, DevOps, Cloud, Go/Golang, Typescript, Docker, Kubernetes, and much more!"
-    />
-    <StyledP>Blog page</StyledP>
-    <Link to="/my-first-post">My First post</Link>
-  </Layout>
-)
+type BlogPageProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: [
+        {
+          node: {
+            frontmatter: Post
+          }
+        }
+      ]
+    }
+  }
+}
 
+const PostPreview = ({ title, description, date, path }: Post): JSX.Element => {
+  return <Link to={path}>{title}</Link>
+}
+
+const BlogPage = ({ data }: BlogPageProps): JSX.Element => {
+  return (
+    <Layout>
+      <Seo
+        title="harrison.me"
+        description="This blog contains posts about what I'm learning as a software engineer. Topics include Javascript, DevOps, Cloud, Go/Golang, Typescript, Docker, Kubernetes, and much more!"
+      />
+      <StyledP>Blog page</StyledP>
+      <div>
+        {data.allMarkdownRemark.edges.map(({ node }, i) => {
+          return <PostPreview {...node.frontmatter} key={i} />
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query BlogPostsQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            path
+            date
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
 export default BlogPage
