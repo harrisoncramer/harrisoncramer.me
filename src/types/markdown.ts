52e898857edf9123467fe8c2f31f66597fb7f9ce
Harrison Cramer <harrisoncramer@gmail.com>
2021-06-18 08:45:18 -0400

    Added image component to post template.

--- src/types/markdown.ts
+++ src/types/markdown.ts
@@ -1,28 +1,13 @@
+import { IGatsbyImageData } from "gatsby-plugin-image"
 export type Post = {
   title: string
   date: string
   path: string
   description: string
+  imageDescription: string
   featuredImage: {
     childImageSharp: {
-      gatsbyImageData: {
-        layout: string
-        backgroundColor: string
-        images: {
-          fallback: {
-            src: string
-            srcSet: string
-            sizes: string
-          }
-          sources: [
-            {
-              srcSet: string
-              type: string
-              sizes: string
-            }
-          ]
-        }
-      }
+      gatsbyImageData: IGatsbyImageData
     }
   }
 }
