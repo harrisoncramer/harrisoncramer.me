0060140bd6f007b588c488bf09d8be14c2d52645
Harrison Cramer <harrisoncramer@gmail.com>
2021-06-24 22:34:04 -0400

    [ci skip] Moved variables into separate terraform.tfvars file.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,15 +4,15 @@
 
 variable "domain_name" {
   description = "Pre-purchased domain name you on Route53 we want the certs for. Mine is my personal blog."
-  default     = "harrisoncramer.me"
+  type        = string
 }
 
 variable "owner" {
   description = "Tag to apply to resources to indicate owner"
-  default     = "Harrison Cramer"
+  type        = string
 }
 
 variable "application" {
   description = "Tag to apply to resources to indicate application"
-  default     = "harrisoncramer.me"
+  type        = string
 }
