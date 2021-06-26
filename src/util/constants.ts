5fb0f01aba9bfd83288e70ed1c624affb66baf41
Harrison Cramer <harrisoncramer@gmail.com>
2021-06-26 09:17:16 -0400

    Added control of tags through constant.

---
+++ src/util/constants.ts
@@ -0,0 +1,11 @@
+// These are the only tags possible.
+// Any other tag will not be filtered out inside the post
+// This constant must match the list of files inside of src/pages/categories
+export const categories = [
+  "aws",
+  "circleci",
+  "docker",
+  "javascript",
+  "react",
+  "terraform",
+]
