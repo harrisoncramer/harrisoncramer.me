570a909b67464d9b1b6fb392565980cdd7742137
Harrison Cramer <harrisoncramer@gmail.com>
2021-06-28 10:08:27 -0400

    removed react-refs post

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,8 +20,8 @@ const Header = (): JSX.Element => {
         isDark={!!isDark}
         onClick={changeTheme}
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
+        width="20"
+        height="20"
         viewBox="0 0 24 24"
       >
         <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10v-20zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z" />
@@ -53,6 +53,7 @@ const StyledHeader = styled.header`
   display: flex;
   padding: 1em;
   gap: 1em;
+  align-items: center;
   box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.1);
 
   justify-content: space-between;
