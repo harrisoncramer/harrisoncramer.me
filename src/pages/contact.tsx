fbba6b9010a08c72d0e4a75bcaaca54deb5925d5
Harrison Cramer <harrisoncramer@gmail.com>
2021-07-07 18:15:19 -0400

    Added contact page.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 
+import ReactTooltip from "react-tooltip"
 import Layout from "../components/layout/layout"
 import Seo from "../components/seo/seo"
 import { StyledH1 } from "../components/styled-components/text"
+import styled from "styled-components"
 
 const ContactPage = (): JSX.Element => (
   <Layout title="contact">
@@ -10,8 +12,51 @@ const ContactPage = (): JSX.Element => (
       title="contact"
       description="This page includes my contact information like my Twitter handle and my Github account and my developer email. It also includes my resume."
     />
-    <StyledH1>Contact page</StyledH1>
+    <StyledH1>Contact</StyledH1>
+    <StyledDiv>
+      <StyledP>
+        Twitter:{" "}
+        <a href="https://twitter.com/harrisoncramer">@harrisoncramer</a>
+      </StyledP>
+    </StyledDiv>
+    <StyledDiv>
+      <StyledP>
+        Github: <a href="https://github.com/harrisoncramer">harrisoncramer</a>
+      </StyledP>
+    </StyledDiv>
+    <StyledDiv>
+      <StyledP data-tip data-for="signal-tip">
+        Signal: 510-698-1166
+      </StyledP>
+    </StyledDiv>
+    <ReactTooltip
+      id="signal-tip"
+      place="right"
+      effect="solid"
+      className="tooltip"
+    >
+      Signal is a secure messenging application.
+    </ReactTooltip>
+    <StyledDiv>
+      <StyledP>
+        Email:{" "}
+        <a href="mailto:kingofcramers.dev@gmail.com">
+          kingofcamers.dev@gmail.com
+        </a>
+      </StyledP>
+    </StyledDiv>
   </Layout>
 )
+
+const StyledDiv = styled.div`
+  .tooltip {
+    font-size: 2em !important;
+  }
+`
+
+const StyledP = styled.p`
+  margin: 0.25em;
+  display: inline-block;
+`
 
 export default ContactPage
