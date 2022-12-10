e4636f85b36f7295182580835a716dadd13fc9c5
Harrison Cramer <harrisoncramer@gmail.com>
2022-12-09 21:35:17 -0500

    Removed resume (for now)

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,7 +9,7 @@ const ContactPage = (): JSX.Element => (
   <Layout title="contact">
     <Seo
       title="contact"
-      description="This page includes my contact information like my Twitter handle and my Github account and my developer email. It also includes my resume."
+      description="This page includes my contact information like my Twitter handle and my Github account and my developer email."
       slug="/contact"
     />
     <h1>Contact</h1>
@@ -42,10 +42,7 @@ const ContactPage = (): JSX.Element => (
       <StyledP>
         I'm always looking for cool projects to work on, particularly in the
         open source community. If you think I could be a good fit for your team,
-        please don't hesitate to reach out. My resume can be found{" "}
-        <a href="https://s3.amazonaws.com/harrisoncramer.me.assets/Cramer_Resume.pdf">
-          here.
-        </a>
+        please don't hesitate to reach out.
       </StyledP>
     </StyledDiv>
   </Layout>
