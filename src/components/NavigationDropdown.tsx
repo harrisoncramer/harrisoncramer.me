bb0f5f7b9f90f0ecd36389790869ef541bd68957
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-18 18:28:37 -0400

    Fixed navigation

--- src/components/NavigationDropdown.tsx
+++ src/components/NavigationDropdown.tsx
@@ -8,8 +8,6 @@ const NavigationDropdown = ({ isDark}: NavigationDropdownProps): JSX.Element => 
   const dropdownRef = useRef(null)
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
-  const handleClickLink = () => setIsDropdownOpen(false)
-
   const pageClickEvent = (e: React.MouseEvent) => {
     if (dropdownRef.current?.contains(e.target)) {
       setIsDropdownOpen(!isDropdownOpen)
@@ -38,11 +36,10 @@ const NavigationDropdown = ({ isDark}: NavigationDropdownProps): JSX.Element => 
       >
         <path d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z" />
       </svg>
-      <ul 
-        className={`mt-4 absolute transition-opacity overflow-hidden ${!isDropdownOpen ? 'opacity-0' : 'opacity-100'} ${isDark ? 'bg-app-black text-app-white' : 'bg-app-white text-app-black'} shadow rounded-md`}>
-        <li className={`px-4 py-2 w-48 relative hover:bg-app-white ${isDropdownOpen && 'cursor-pointer'}`}>Blog</li>
-        <li className={`px-4 py-2 w-48 relative hover:bg-app-white ${isDropdownOpen && 'cursor-pointer'}`}>About</li>
-        <li className={`px-4 py-2 w-48 relative hover:bg-app-white ${isDropdownOpen && 'cursor-pointer'}`}>Contact</li>
+      <ul className={`mt-4 w-48 absolute transition-opacity overflow-hidden ${!isDropdownOpen ? 'opacity-0' : 'opacity-100'} ${isDark ? 'bg-app-black text-app-white' : 'bg-app-white text-app-black'} shadow rounded-md`}>
+        <a className={`block relative hover:bg-app-white ${isDropdownOpen && 'cursor-pointer'} px-4 py-2 w-full` } href="/blog">Blog</a>
+        <a className={`block relative hover:bg-app-white ${isDropdownOpen && 'cursor-pointer'} px-4 py-2 w-full` } href="/about">About</a>
+        <a className={`block relative hover:bg-app-white ${isDropdownOpen && 'cursor-pointer'} px-4 py-2 w-full` } href="/contact">Contact</a>
       </ul>
     </nav>
   )
