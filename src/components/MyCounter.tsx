09c25476a469a6cc2884be304312bab3df816302
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-25 15:30:07 -0400

    Embedding TSX components directly into the page

---
+++ src/components/MyCounter.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+interface CounterProps {
+  initialCount?: number;
+}
+
+const Counter: React.FC<CounterProps> = ({ initialCount = 0 }) => {
+  const [count, setCount] = useState<number>(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <div className="gap-1 flex">
+        <button className="bg-app-white text-app-black w-6 rounded-sm" onClick={decrement}>-</button>
+        <button className="bg-app-white text-app-black w-6 rounded-sm" onClick={increment}>+</button>
+      <p>Count: {count}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
