66d8dd0194be7d97bc0700cbb4dfcab647570179
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-27 21:42:34 -0400

    Set up Post Preview component

--- src/components/NavigationDropdown.tsx
+++ src/components/NavigationDropdown.tsx
@@ -32,7 +32,7 @@ const NavigationDropdown = (): JSX.Element => {
       >
         <path d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z" />
       </svg>
-      <ul className={`mt-4 w-48 absolute transition-opacity overflow-hidden ${!isDropdownOpen ? 'opacity-0' : 'opacity-100'} bg-app-black text-app-white shadow rounded-md`}>
+      <ul className={`mt-4 w-48 absolute transition-opacity overflow-hidden ${!isDropdownOpen ? 'opacity-0' : 'opacity-100 shadow-app-background-light shadow-md'} bg-app-black text-app-white shadow rounded-md`}>
         <li>
           <a className={`block text-xl relative hover:bg-app-background-light text-app-white ${isDropdownOpen && 'cursor-pointer'} px-4 py-2 w-full` } href="/"><h3>Blog</h3></a>
         </li>
