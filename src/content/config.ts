5787e0aa0defeedd0ffdf79743f32a8bb68d9ec1
Harrison Cramer <harrisoncramer@gmail.com>
2023-03-29 20:31:32 -0400

    Set up tag constants

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { TAGS } from '../consts';
 
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
@@ -16,9 +17,7 @@ const blog = defineCollection({
 			.transform((str) => (str ? new Date(str) : undefined)),
     imageDescription: z.string(),
 		heroImage: z.string().optional(),
-    tags: z
-    .array(z.string())
-    .optional()
+    tags: z.array(z.enum(TAGS)).optional()
 	}),
 });
 
