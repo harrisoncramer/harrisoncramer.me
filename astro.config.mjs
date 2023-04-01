821c2d21d83f96f6754ac1252f26e097afb6adc9
Harrison Cramer <harrisoncramer@gmail.com>
2023-04-01 13:18:38 -0400

    Added code syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,28 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const prettyCodeOptions = {
+  theme: "dark-plus",
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word"];
+  },
+  tokensMap: {},
+};
 
 import image from "@astrojs/image";
 
@@ -11,5 +33,10 @@ export default defineConfig({
   site: 'https://harrisoncramer.me',
   integrations: [mdx(), sitemap(), react(), tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  })],
+  markdown: {
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+  },
 });
