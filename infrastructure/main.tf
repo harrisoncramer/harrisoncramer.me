414321f3f4a370511c86d155289445176ecdc96a
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-14 11:14:38 -0400

    Updated infrastructure/main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,9 +3,8 @@
 ######################
 
 provider "aws" {
-  region                  = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "terraform"
+  region  = "us-east-1"
+  profile = "harrison"
 }
 
 locals {
