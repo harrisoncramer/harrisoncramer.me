cdf44650ad72e4322c11541099a2d8032d560c30
Harrison Cramer <harrisoncramer@gmail.com>
2026-09-24 02:56:21 -0400

    fix: not full width

--- src/components/NavigationDropdown.tsx
+++ src/components/NavigationDropdown.tsx
@@ -68,7 +68,7 @@ const NavigationDropdown = ({ links, path }: NavigationDropdownProps): JSX.Eleme
       </button>
       <ul
         id={MENU_ID}
-        className={`absolute left-0 right-0 top-full border-y border-app-background-medium bg-app-background-dark pb-3 pt-2 shadow-3xl ${isOpen ? "visible [clip-path:inset(0_0_-1rem_0)] [transition:clip-path_220ms_cubic-bezier(0.16,1,0.3,1),visibility_0s]" : "invisible [clip-path:inset(0_0_100%_0)] [transition:clip-path_160ms_cubic-bezier(0.7,0,0.84,0),visibility_0s_160ms]"}`}
+        className={`absolute right-0 top-full min-w-[180px] border border-app-background-medium rounded-b-md bg-app-background-dark pb-3 pt-2 shadow-3xl ${isOpen ? "visible [clip-path:inset(0_0_-1rem_0)] [transition:clip-path_220ms_cubic-bezier(0.16,1,0.3,1),visibility_0s]" : "invisible [clip-path:inset(0_0_100%_0)] [transition:clip-path_160ms_cubic-bezier(0.7,0,0.84,0),visibility_0s_160ms]"}`}
       >
         {links.map((link) => (
           <li key={link.href}>
